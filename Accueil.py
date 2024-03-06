38047ffc8f4a55b08efefb55fbe63b32990de61b
bflorestal <yayou7710@hotmail.fr>
2024-03-06 18:37:47 +0100

    Ajout de texte sur l'accueil

--- Accueil.py
+++ Accueil.py
@@ -5,4 +5,9 @@ st.set_page_config(
     page_icon="🔫",
 )
 
-st.write("# Détection d'armes")
+st.title("Détection d'armes")
+
+st.write("Bienvenue sur l'application de détection d'armes. Vous pouvez choisir de détecter des armes dans des images ou dans des vidéos.")
+st.write("Pour commencer, veuillez choisir une option dans le menu de gauche.")
+
+st.write("Pour plus d'informations, veuillez consulter le [GitHub](https://github.com/pascal-rithi-ke/detection_arme).")
