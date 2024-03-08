231e233258c1f87a41fa2bec84560d4d62fab67b
adbiggy <mirabelle.adrien@sfr.fr>
2024-03-08 09:34:53 +0100

    Détection Image InceptionV3

--- pages/1_Detection_Image.py
+++ pages/1_Detection_Image.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import cv2
 import numpy as np
+from tensorflow.keras.models import load_model
+
+# Charger le modèle CNN pré-entraîné
+model = load_model('C:/Users/33749/Desktop/IPSSI/MachineLearning_DeepLearning/Projet_Detection_Arme/detection_arme/Notebook/inceptionv3_model.h5')
+
+# Définir les catégories de classe
+classes = ['Gun', 'Knife', 'Rifle']
 
 st.set_page_config(
     page_title="Images - Détection d'armes",
@@ -14,4 +21,16 @@ if uploaded_image is not None:
     file_bytes = np.asarray(bytearray(uploaded_image.read()), dtype=np.uint8)
     image = cv2.imdecode(file_bytes, 1)
     
+    # Prétraitement de l'image
+    resized_image = cv2.resize(image, (150, 150))  # Redimensionner l'image pour correspondre à la taille d'entrée du modèle
+    normalized_image = resized_image / 255.0  # Normaliser les valeurs des pixels dans la plage [0, 1]
+    
+    # Classification de l'image
+    prediction = model.predict(np.expand_dims(normalized_image, axis=0))[0]
+    predicted_class_index = np.argmax(prediction)
+    predicted_class = classes[predicted_class_index]
+    confidence = prediction[predicted_class_index]
+    
+    # Affichage des résultats
     st.image(image, channels="BGR", caption="Image téléchargée")
+    st.write(f"Prédiction : {predicted_class} avec une confiance de {confidence:.2f}")
